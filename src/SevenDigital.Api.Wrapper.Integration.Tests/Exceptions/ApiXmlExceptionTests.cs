c5d4bd5b5ee46aa7f331a2ff7f7b78666ff32fb2
AnthonySteele <Anthony@PlasticAvalon.org>
2012-09-16 19:53:16 +0100

    Async'd two tests that were failing for lack of it. they might pass now, but we haven't solved the problem yet of sync tests in general so we don't know for sure.

--- src/SevenDigital.Api.Wrapper.Integration.Tests/Exceptions/ApiXmlExceptionTests.cs
+++ src/SevenDigital.Api.Wrapper.Integration.Tests/Exceptions/ApiXmlExceptionTests.cs
@@ -10,22 +10,44 @@ namespace SevenDigital.Api.Wrapper.Integration.Tests.Exceptions
 	public class ApiXmlExceptionTests
 	{
 		[Test]
-		public void Should_fail_correctly_if_xml_error_returned()
+		public async void Should_fail_correctly_if_xml_error_returned()
 		{
 			// -- Deliberate error response
 			Console.WriteLine("Trying artist/details without artistId parameter...");
-			var apiXmlException = Assert.Throws<ApiXmlException>(() => Api<Artist>.Create.PleaseAsync());
 
+			ApiXmlException apiXmlException = null;
+
+			try
+			{
+				await Api<Artist>.Create.PleaseAsync();
+			}
+			catch (ApiXmlException ex)
+			{
+				apiXmlException = ex;
+			}
+
+			Assert.That(apiXmlException, Is.Not.Null);
 			Assert.That(apiXmlException.Error.Code, Is.EqualTo(1001));
 			Assert.That(apiXmlException.Error.ErrorMessage, Is.EqualTo("Missing parameter artistId."));
 		}
 
 		[Test]
-		public void Should_fail_correctly_if_non_xml_error_returned_eg_unauthorised()
+		public async void Should_fail_correctly_if_non_xml_error_returned_eg_unauthorised()
 		{
 			// -- Deliberate unauthorized response
 			Console.WriteLine("Trying user/locker without any credentials...");
-			var apiXmlException = Assert.Throws<ApiXmlException>(() => Api<Locker>.Create.PleaseAsync());
+			ApiXmlException apiXmlException = null;
+
+			try
+			{
+				await Api<Locker>.Create.PleaseAsync();
+			}
+			catch (ApiXmlException ex)
+			{
+				apiXmlException = ex;
+			}
+
+			Assert.That(apiXmlException, Is.Not.Null);
 			Assert.That(apiXmlException.Error.Code, Is.EqualTo(9001));
 			Assert.That(apiXmlException.Error.ErrorMessage, Is.EqualTo("OAuth authentication error: Resource requires access token"));
 		}
