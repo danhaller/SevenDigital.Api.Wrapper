bdafac9ae78c90d89fbdf0adf39425ff56db7752
greg Sochanik <greg@sochanik.com>
2012-11-15 15:55:55 +0000

    merging

--- src/SevenDigital.Api.Wrapper/FluentApi.cs
+++ src/SevenDigital.Api.Wrapper/FluentApi.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Net.Http;
-using System.Linq;
 using System.Threading.Tasks;
-using SevenDigital.Api.Schema.OAuth;
+using SevenDigital.Api.Wrapper.AttributeManagement;
 using SevenDigital.Api.Wrapper.EndpointResolution;
 using SevenDigital.Api.Wrapper.EndpointResolution.OAuth;
-using SevenDigital.Api.Schema.Attributes;
-using SevenDigital.Api.Wrapper.Exceptions;
 using SevenDigital.Api.Wrapper.Http;
 using SevenDigital.Api.Wrapper.Serialization;
 
@@ -15,54 +11,18 @@ namespace SevenDigital.Api.Wrapper
 {
 	public class FluentApi<T> : IFluentApi<T> where T : class
 	{
-		private readonly RequestData _requestData = new RequestData();
+		private readonly RequestData _requestData;
 		private readonly IRequestCoordinator _requestCoordinator;
 		private readonly IResponseParser<T> _parser;
 
 		public FluentApi(IRequestCoordinator requestCoordinator)
 		{
+			var attributeValidation = new AttributeValidation<T>();
+			_requestData = attributeValidation.Validate();
+
 			_requestCoordinator = requestCoordinator;
 
 			_parser = new ResponseParser<T>();
-
-			ApiEndpointAttribute attribute = typeof(T).GetCustomAttributes(true)
-				.OfType<ApiEndpointAttribute>()
-				.FirstOrDefault();
-
-			if (attribute == null)
-			{
-				throw new ArgumentException(string.Format("The Type {0} cannot be used in this way, it has no ApiEndpointAttribute", typeof(T)));
-			}
-
-			_requestData.UriPath = attribute.EndpointUri;
-
-
-			OAuthSignedAttribute isSigned = typeof(T).GetCustomAttributes(true)
-				.OfType<OAuthSignedAttribute>()
-				.FirstOrDefault();
-
-			if (isSigned != null)
-			{
-				_requestData.IsSigned = true;
-			}
-
-			RequireSecureAttribute isSecure = typeof(T).GetCustomAttributes(true)
-				.OfType<RequireSecureAttribute>()
-				.FirstOrDefault();
-
-			if (isSecure != null)
-			{
-				_requestData.UseHttps = true;
-			}
-
-			HttpPostAttribute isHttpPost = typeof(T).GetCustomAttributes(true)
-				.OfType<HttpPostAttribute>()
-				.FirstOrDefault();
-
-			if (isHttpPost != null)
-			{
-				_requestData.HttpMethod = HttpMethod.Post;
-			}
 		}
 
 		public FluentApi(IOAuthCredentials oAuthCredentials, IApiUri apiUri)
